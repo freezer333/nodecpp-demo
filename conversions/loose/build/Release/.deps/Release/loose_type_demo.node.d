200ee16d92d7595a3410bf9841b91c3cf85945c6
Scott Frees <scott.frees@gmail.com>
2016-04-02 08:19:07 -0400

    updated ignore

--- conversions/loose/build/Release/.deps/Release/loose_type_demo.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/loose_type_demo.node := c++ -bundle -undefined dynamic_lookup -Wl,-search_paths_first -mmacosx-version-min=10.5 -arch x86_64 -L./Release  -o Release/loose_type_demo.node Release/obj.target/loose_type_demo/loose_type_demo.o 
--- conversions/loose/build/Release/.deps/Release/obj.target/loose_type_demo.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/obj.target/loose_type_demo.node := g++ -shared -pthread -rdynamic -m64  -Wl,-soname=loose_type_demo.node -o Release/obj.target/loose_type_demo.node -Wl,--start-group Release/obj.target/loose_type_demo/loose_type_demo.o -Wl,--end-group 

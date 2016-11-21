87d205551a8a447cee63b9554fd0d1e3aad2b4f9
Scott Frees <scott.frees@gmail.com>
2016-11-21 12:34:47 -0500

    updated buffers example to use length

--- buffers/basic/buffer_example.cpp
+++ buffers/basic/buffer_example.cpp
@@ -4,8 +4,8 @@ using namespace v8;
 
 NAN_METHOD(rotate) {
     char* buffer = (char*) node::Buffer::Data(info[0]->ToObject());
-    unsigned int size = info[1]->Uint32Value();
-    unsigned int rot = info[2]->Uint32Value();
+    size_t size = node::Buffer::Length(info[0]);
+    unsigned int rot = info[1]->Uint32Value();
    
     char * retval = new char[size];
     for(unsigned int i = 0; i < size; i++ ) {
--- buffers/basic/index.js
+++ buffers/basic/index.js
@@ -4,7 +4,7 @@ const addon = require('./build/Release/buffer_example');
 const buffer = Buffer.from("ABC");
 
 // synchronous, rotates each character by +13
-var result = addon.rotate(buffer, buffer.length, 13);
+var result = addon.rotate(buffer, 13);
 
 console.log(buffer.toString('ascii'));
 console.log(result.toString('ascii'));
